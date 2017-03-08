513fd8e54d61726848003de2c7ff74b01d595eae
AngryMajor <peterfmajor@gmail.com>
2017-03-08 16:09:58 +0000

    Update Main.c
    creating a pseudo code plan

--- Main.c
+++ Main.c
@@ -1,6 +1,21 @@
 include<stdio.h>
-include"OurTypes.c"
-
+include"OurTypes.h"
+// creat header for func prototypes
+  
 int main(){
 
+  int choise
+  
+  //get input from user how many players
+  for(up to choise){
+  //function to creat plaers geting input for what each player is
+   //returns a player witch we stor in an array
+  }
+  
+  //same thing for map slots
+  
+  for(number of players){
+  //creat menue for turns
+  }
+    
 }//end of main
