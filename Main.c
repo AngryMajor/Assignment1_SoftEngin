7c03e57a07352e1a6a9fd62271baeb894c553ee0
mgrogan0058 <matthew.grogan@ucdconnect.ie>
2017-03-08 16:21:36 +0000

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,15 +2,23 @@ include<stdio.h>
 include"OurTypes.h"
 // creat header for func prototypes
   
-int main(){
+void elf(struct player *Current_Player);  //Function prototype
+void human(struct player *Current_Player);   //Function prototype
+void ogre(struct player *Current_Player);   //Function prototype
+void wizard(struct player *Current_Player);   //Function prototype 
+void type();
 
-  int choise
-  
-  //get input from user how many players
-  for(up to choise){
-  //function to creat plaers geting input for what each player is
+int main()
+{
+int choice;
+printf("Please input how many players are playing the game: ");
+scanf("%d", &choice);
+struct player player[choice];
+ for(int i = 0; i < choice; i++)
+ {
+   type();
    //returns a player witch we stor in an array
-  }
+ }
   
   //same thing for map slots
   
