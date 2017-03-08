8292151408edaa612fac540a852f4ab2e74c7a4f
mgrogan0058 <matthew.grogan@ucdconnect.ie>
2017-03-08 18:49:53 +0000

    Create Attack.c

---
+++ Attack.c
@@ -0,0 +1,18 @@
+include "OurFunctions.h"
+include "OurTypes.h"
+
+void attack(struct player *AttackerPlayer, struct player *AttackedPlayer)
+{
+	if(AttackedPlayer->strength <=70)
+	{
+		AttackedPlayer->life_points = AttackedPlayer->life_points - (0.5 * AttackedPlayer->strength);
+		printf("1. Attacked Player Life points = %d", AttackedPlayer->life_points);
+	}
+	
+	else if(AttackedPlayer->strength > 70)
+	{
+		AttackerPlayer->life_points = AttackerPlayer->life_points - (0.3 * AttackedPlayer->strength);
+		printf("2. Attcker Player Life Points = %d", AttackerPlayer->life_points);
+	}
+	
+}
