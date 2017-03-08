608e76a0909f14e4e795e9d2e523b6c675ee2993
AngryMajor <peterfmajor@gmail.com>
2017-03-08 12:26:29 +0000

    Update MoveFunction.c
    removed main and the type declarations as I've moved them to another file and added the include for the type header

--- MoveFunction.c
+++ MoveFunction.c
@@ -1,42 +1,14 @@
 #include <stdio.h>
+#include "OurTypes.h"
 
 #define PlayerLocation (*Player).index
 
-/*
-Dummy Player strut for testing
-*/
 
-struct PlayerStruct{
-	
-	int index;
-	
-};//nd of player struct
-
-typedef struct MapSlot {
-	
-	int SlotType;//stors an int represening the slot type, 0=flat ground, 1=hill, 2=city
-	int PlayerHere;//stors the index from the player list of the player who is in this slot, 
-	
-}MapSlot;//end of map slot struct
 
 
 void EnterSlot(struct PlayerStruct *Player, int SlotType);
 void ExitSlot(struct PlayerStruct *Player, int SlotType);
 void Move(struct PlayerStruct *Player, MapSlot MapSlots[], int EnterSlot, int ExitSlot);
-
-
-int main(){
-	
-	int MapSize = 3;
-	
-	MapSlot MapSlots[MapSize];
-	struct PlayerStruct Dummy;
-	Dummy.index = 1;
-	
-	int MoveTo= 2;
-	
-	Move(&Dummy, MapSlots, MoveTo, Dummy.index);
-}//end of main
 
 
 void Move(struct PlayerStruct *Player, MapSlot MapSlots[], int enterSlot, int exitSlot){
