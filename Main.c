d1a068f8fe168ce1cd8f1070459250f06c159cdd
mgrogan0058 <matthew.grogan@ucdconnect.ie>
2017-03-08 17:53:28 +0000

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,22 @@
+include "OurFunctions.h"
+include "OurTypes.h"
+
+int main(void)
+{
+int choice;
+printf("Please input how many players are playing the game: ");
+scanf("%d", &choice);
+struct player player[choice];
+ for(int i = 0; i < choice; i++)
+ {
+   type();
+   //returns a player witch we stor in an array
+ }
+  
+  //same thing for map slots
+  
+  for(number of players){
+  //creat menue for turns
+  }
+    
+}//end of main
