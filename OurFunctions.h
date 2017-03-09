4cfa8c29de58264c361f77c352092efc4defe71c
mgrogan0058 <matthew.grogan@ucdconnect.ie>
2017-03-09 17:34:38 +0000

    Update OurFunctions.h

--- OurFunctions.h
+++ OurFunctions.h
@@ -27,7 +27,7 @@ int find_players(struct player player[], int i, MapSlot map[], int MapSize);
 
 void attack(struct player *AttackerPlayer, struct player *AttackedPlayer);
 
-void PrintMapSlotType(Map[i]);
+void PrintMapSlotType(MapSlots Map[i]);
 
 MapSlot CreatMapSlot();
 
