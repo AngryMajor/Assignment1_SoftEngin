d0679fdaf2cef7687bbb0583748173580e2d6e14
AngryMajor <peterfmajor@gmail.com>
2017-03-09 17:09:50 +0000

    Update MapLogic.c

--- MapLogic.c
+++ MapLogic.c
@@ -1,98 +1,59 @@
 #include "OurFunctions.h"
 
-/*
-Calls ExitSlot then changes data of were the player and finaly calles enter slot
-needs a player the map array and the directions Left is less, right is more
-returns nothing
-*/
-
-int Move(struct player *Player,int PlayerIndex, MapSlot MapSlots[], int direction){
-	
-	if((MapSlots[((*Player).mapslot)+direction]).PlayerHere != -1){
-		
-		//if location is full
-		printf("That Location is ocupied");
-		return 1;//returning 1 says to main that this didnt work
-		
-	}//end of ocupied check
-	
-	
-	ExitSlot(Player, (*Player).mapslot);//change player stats for base
-	
-	//move player in the data
-	(*Player).mapslot += direction;
-	(MapSlots[((*Player).mapslot) - direction]).PlayerHere = -1;
-	(MapSlots[(*Player).mapslot]).PlayerHere = PlayerIndex;
-	
-	EnterSlot(Player, (*Player).mapslot);//change player stats to location modified
-	
-	return 0;//move exicuted fine
-	
-}  //End of move
-
-//Defines to make it easyer to change player struct
-#define PlayerDex (*Player).dexterity
-#define PlayerStr (*Player).strength
-#define PlayerSmar (*Player).smartness
-#define PlayerMag (*Player).magic_skills
-
 
 /*
-Makes aproprate changes to player stats when then enter a new slot
-requires a pointer to the player struct and slot being entered 
-modifies the player struct in acordence to the slot it is entering
+Places each player in a random index location in the map
+takes the map and player arrays as well as ints of there sizes as input
+returns nothing but changes the location variables in the map and the players
+note srand must be called before running this function and all map slots must have their player here set to -1 inishaly
+also check that there are not more players then slots
 */
-
-void EnterSlot(struct player *Player, int SlotType){
+void PlacePlayer(struct player Players[], int playersSize, struct MapSlot MapArray[], int MapSize){
 	
-	
-	
-	switch(SlotType){
-		case 0:  //If flat land
-			break;
-			
-		case 1:  //If hill
-			if(PlayerDex < 50) PlayerStr -=10;  
-			else if(PlayerDex >= 60) PlayerStr +=10;
-			break;
-			
-		case 2:  //If city
-			if( PlayerSmar > 60) PlayerMag +=10;
-			if( PlayerSmar <= 50) PlayerDex -=10;
-			break;
+	int playerNum=0;
+	int PlayerPlace;
 		
-		default:
-			printf("invlad slotType on enter");
-	}  //End of switch
-	
-	
-}//end of enter slot
-
-
-/*
-reverts a players stats to what they were before entering the slot
-requires a pointer to the player struct and slot being exited 
-modifies the player struct in acordence to the slot it is entering
-*/
-
-void ExitSlot(struct player *Player, int SlotType){
-	
-	switch(SlotType){
-		case 0://if flat land
-			break;
-			
-		case 1://if hill
-			if(PlayerDex < 50) PlayerStr +=10;  
-			else if(PlayerDex >= 60) PlayerStr -=10;
-			break;
-			
-		case 2://if city
-			if( PlayerSmar > 60) PlayerMag -=10;
-			if( PlayerSmar<= 50) PlayerDex +=10;
-			break;
+	while(playerNum<playersSize){
 		
-		default:
-			printf("invlad slotType on exit");
-	}//end of switch
+		//Clear player place data
+		Players[playerNum].mapslot = -1;
+		
+		//Store posable locatoin for player to be put
+		PlayerPlace = rand()%MapSize;
+		
+		//Check if that location has a player in it, if not then put the player there
+		while(Players[playerNum].mapslot == -1){
+			
+			if(MapArray[PlayerPlace].PlayerHere  == -1){
+				
+				MapArray[PlayerPlace].PlayerHere = playerNum;
+				Players[playerNum].mapslot = PlayerPlace;
+				playerNum++;
+			}else{
+				PlayerPlace = ((PlayerPlace+1)%3);}
+		}  //End of while
+		
+		EnterSlot(&Players[playerNum],PlayerPlace);
+		
+	}  //End of i loop
+}  //End of place player
+
+
+
+void PrintMapSlotType(MapSlot toPrint){
 	
-}//end of exit slot
+	char *SlotNames[] = {"Flat Ground","Hill","City"};
+	
+	printf("%s",SlotNames[toPrint.thisSlotType]);
+	
+}  //End of printmapslot
+
+
+MapSlot CreatMapSlot(){
+	MapSlot Curr;
+	
+	Curr.thisSlotType = rand()%3;
+	Curr.PlayerHere = -1;
+	
+	return Curr;
+}  //End of creat map slot
