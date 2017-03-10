311fbd8d8dea77d096ecc6ebc80e788800068940
AngryMajor <peterfmajor@gmail.com>
2017-03-10 10:30:31 +0000

    Update FillMap_testMain.c

--- FillMap_testMain.c
+++ FillMap_testMain.c
@@ -27,3 +27,9 @@ void PrintMapSlotChar(MapSlot toPrint){
 	if(toPrint.PlayerHere != -1) printf(   "%d\n",toPrint.PlayerHere);
 	puts("");
 }  //End of printmapslot
+
+void PrintPlayerStats(struct player Player){
+	
+	printf("type:  %d\nname:  %d\nlife points: %d\nsmartness:  %d\nstrength:  %d\nmagic skills:  %d\nluck:  %d\ndexterity:  %d\n",Player.type, Player.name, Player.life_points, Player.smartness, Player.strength, Player.magic_skills, Player.luck, Player.dexterity);
+	
+}//end of print player stats
