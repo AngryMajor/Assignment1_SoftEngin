87998cafd862d235be93c164076c10c28b0a5f0b
AngryMajor <peterfmajor@gmail.com>
2017-03-10 10:53:01 +0000

    Update Attack.c

--- Attack.c
+++ Attack.c
@@ -5,13 +5,13 @@ void attack(struct player *AttackerPlayer, struct player *AttackedPlayer)
 	if(AttackedPlayer->strength <=70)
 	{
 		AttackedPlayer->life_points = AttackedPlayer->life_points - (0.5 * AttackedPlayer->strength);
-		printf("1. Attacked Player Life points = %d\n", AttackedPlayer->life_points);
+		printf("1. Attacked Player Life points = %d\n\n", AttackedPlayer->life_points);
 	}
 	
 	else if(AttackedPlayer->strength > 70)
 	{
 		AttackerPlayer->life_points = AttackerPlayer->life_points - (0.3 * AttackedPlayer->strength);
-		printf("2. Attcker Player Life Points = %d\n", AttackerPlayer->life_points);
+		printf("2. Attcker Player Life Points = %d\n\n", AttackerPlayer->life_points);
 	}
 	
 }
